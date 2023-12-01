b55fdaaa9d84ed7f0b3c7714b30bca9c783bff09
sunsijia0801 <64207618+sunsijia0801@users.noreply.github.com>
2023-12-01 15:07:26 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,66 +1,50 @@
 /* eslint-disable */
-
-/**
- * 此项目配置为方便新人使用，已缩减至最简配置。
- * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
- * 自行添加属性，以支持更多个性化功能
- */
 const USER_CONFIG = {
+  /**
+   * 基本配置
+   */
+  
+  // 使用的推送通道：['push-deer', 'wechat-test', 'server-chan', 'push-plus']
+  // 默认使用 【微信测试号】
+  // 使用【pushDeer】请填写 push-deer
+  // 使用【微信测试号】请填写 wechat-test
+  // 使用【方糖服务号】请填写 server-chan
+  // 使用【pushplus推送加服务号】请填写 push-plus
+  USE_PASSAGE: 'wechat-test',
+  
+  // 使用微信测试号时才需要填写：公众号APP_ID
+  APP_ID: 'wx1d6df526b0f62406',
 
-  // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  // 使用微信测试号时才需要填写：公众号APP_SECRET
+  APP_SECRET: '66ee28bb9411e233bcedfd5323239811',
+  
+  /** 是否给文字设置多彩颜色, 和emoji不兼容 */
+  // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
+  IS_SHOW_COLOR: true
 
-  // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
-
-  PROVINCE: '广东',
-  CITY: '惠州',
-
+  /**
+   * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
+   */
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '程程',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      // 使用其他通道时，请严格按照各个通道的教程进行填写
+      id: 'oo66g6UgPTD_cSeJE-z69a3NPiKI	',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
+      // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
+      useTemplateId: 'r5l0JQ1-ldZFxq0cYXClwimfDp4g4a0xwH5cUUDAViE	',
+      // 所在省份或城市，也可以不填
+      province: '北京',
+      // 所在城市或县区
+      city: '海淀',
+
+      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        // 第一次见面的日子
+        { keyword: 'earthy_love_words', date: '2023-11-15' },
       ],
-    },
-  ],
-
-
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
-
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-    }
-  ],
-
 }
 
 module.exports = USER_CONFIG
