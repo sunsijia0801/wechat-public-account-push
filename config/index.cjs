01b8a6252557ab6499f0b2465338b35d4ebccee3
sunsijia0801 <64207618+sunsijia0801@users.noreply.github.com>
2023-12-01 15:13:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -31,10 +31,10 @@ const USER_CONFIG = {
       name: '程程',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       // 使用其他通道时，请严格按照各个通道的教程进行填写
-      id: 'oo66g6UgPTD_cSeJE-z69a3NPiKI	',
+      id: 'oo66g6UgPTD_cSeJE-z69a3NPiKI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: 'r5l0JQ1-ldZFxq0cYXClwimfDp4g4a0xwH5cUUDAViE	',
+      useTemplateId: 'r5l0JQ1-ldZFxq0cYXClwimfDp4g4a0xwH5cUUDAViE',
       // 所在省份或城市，也可以不填
       province: '北京',
       // 所在城市或县区
