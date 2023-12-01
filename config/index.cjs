82ac892dac200556df5e7114567a3ca1a8a257b6
sunsijia0801 <64207618+sunsijia0801@users.noreply.github.com>
2023-12-01 15:42:26 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oo66g6UgPTD_cSeJE-z69a3NPiKI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'W4IQgh6pnUl5Fu2lk2NFVyz9VWUEnY0Q9NTTY8pIMPw',
+      useTemplateId: '9kVlF8wMm31187Olhw2EQhh_NdnYZnJJPQHn9z29Asw',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
